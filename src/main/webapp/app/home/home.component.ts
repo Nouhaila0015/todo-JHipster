5fd3b0832d02dd93c428b734d9a02ca9b68e9d03
Outmane MGHANEN <outmanemghanen@MacBookPro.lan>
2025-01-17 13:34:02 +0100

    Page d'acceuil

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, inject, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject, signal, Renderer2 } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -11,21 +11,31 @@ import { Account } from 'app/core/auth/account.model';
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  standalone: true,
   imports: [SharedModule, RouterModule],
 })
 export default class HomeComponent implements OnInit, OnDestroy {
   account = signal<Account | null>(null);
 
   private readonly destroy$ = new Subject<void>();
+  private readonly renderer = inject(Renderer2);
 
   private readonly accountService = inject(AccountService);
   private readonly router = inject(Router);
 
   ngOnInit(): void {
+    // Ajouter la classe pour la page d'accueil
+    this.renderer.addClass(document.body, 'home-page');
+
     this.accountService
       .getAuthenticationState()
       .pipe(takeUntil(this.destroy$))
-      .subscribe(account => this.account.set(account));
+      .subscribe(account => {
+        this.account.set(account);
+        if (account) {
+          this.router.navigate(['/todo']);
+        }
+      });
   }
 
   login(): void {
@@ -33,6 +43,8 @@ export default class HomeComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
+    // Retirer la classe quand on quitte la page
+    this.renderer.removeClass(document.body, 'home-page');
     this.destroy$.next();
     this.destroy$.complete();
   }
